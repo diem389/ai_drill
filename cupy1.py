847b7b7944c49c99f487e9892994657cd0f9de70
diem389 <diem389@gmail.com>
2019-03-23 00:20:53 +0900

    add cupy1.py

---
+++ cupy1.py
@@ -0,0 +1,20 @@
+import cupy as cp
+
+x = cp.arange(6, dtype='f').reshape(2, 3)
+y = cp.arange(3, dtype='f')
+
+kernel = cp.ElementwiseKernel(
+    'float32 x, float32 y', 'float32 z',
+    '''
+    if (x - 2 > y) 
+    {
+      z = x * y;
+    } 
+    else 
+    {
+       z = x + y;
+    } 
+    ''', 'kernel1')
+
+r = kernel(x, y)
+print(cp.asnumpy(r))
